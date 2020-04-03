507d8b6f76093055b145859c24727abb915af7f9
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-03 21:01:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(hash)
   	hash.each do |key, value| 
     	if min_value >= value 
             min_key = key
-     	 return min_key
+      end
     end
-  end
+  return min_key  
 end
